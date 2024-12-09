0991234552941d6ffc73bfd92cc090ddba34f563
BioCidee <rudy.courtois@hotmail.com>
2024-12-09 17:35:13 +0100

    WIP Map generator
    - Now generate random level of height
    - Seed System

--- Assets/Games/Scripts/Maps/MapsGeneration/MapGenerator.cs
+++ Assets/Games/Scripts/Maps/MapsGeneration/MapGenerator.cs
@@ -3,31 +3,68 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    [SerializeField] private int height;
+    //[SerializeField] private int height;
+    [SerializeField] private int customSeed = 0;
+    [SerializeField] private int length;
     [SerializeField] private int width;
     [SerializeField] private int depth;
     [SerializeField] private int offset;
     [SerializeField] private Transform pointZero;
     [SerializeField] private GameObject groundParent;
     [SerializeField] private GameObject ground;
+    [SerializeField] private GameObject point;
+    [SerializeField] private float scale = 0.1f;
+    [SerializeField] private int maxHeight;
 
     private List<GameObject> groundList = new List<GameObject>();
 
     private void Start() {
-        Generation();
+        MapGeneration();
     }
 
-    private void Generation() {
-        for (int h = 0; h < height; h++) {
-            for (int w = 0; w < width; w++) {
-                GameObject newGround = Instantiate(ground);
-                newGround.transform.parent = groundParent.transform;
-                newGround.transform.position = new Vector3((1 * w) + offset, 0, (1 * h) + offset);
+    private void MapGeneration() {
+        float mapSeed = (customSeed > 0) ? customSeed : Random.Range(0f, 5000f);
 
-                groundList.Add(newGround);
-            }
+        if (groundList.Count > 0) {
+            ClearCurrentGroundList();
         }
 
-        
+        for (int l = 0; l < length; l++) {
+            for (int w = 0; w < width; w++) {
+                GameObject myGround = GenerateBase(w, l);
+
+                GenerateHeight(w, l, mapSeed, myGround);
+            }
+        } 
+    }
+
+    private void ClearCurrentGroundList() {
+        foreach (GameObject go in groundList) {
+            Destroy(go);
+        }
+
+        groundList.Clear();
+    }
+
+    private GameObject GenerateBase(int w, int l) {
+        GameObject newGround = Instantiate(ground);
+        newGround.transform.parent = groundParent.transform;
+        newGround.transform.position = new Vector3((1 * w) + offset, 0, (1 * l) + offset);
+        groundList.Add(newGround);
+
+        return newGround;
+    }
+
+    private void GenerateHeight(int w, int l, float _mapSeed, GameObject currentGround) {
+        float noiseValue = Mathf.PerlinNoise((l * scale) + _mapSeed, (w * scale) + _mapSeed);
+        int height = Mathf.FloorToInt(noiseValue * maxHeight);
+        height--;
+
+        for (int h = 0; h < height; h++) {
+            GameObject newGroundHeight = Instantiate(ground);
+            newGroundHeight.transform.parent = currentGround.transform;
+            newGroundHeight.transform.position = new Vector3((1 * w) + offset, ((1 * h) + 1) + offset, (1 * l) + offset);
+            groundList.Add(newGroundHeight);
+        }
     }
 }
