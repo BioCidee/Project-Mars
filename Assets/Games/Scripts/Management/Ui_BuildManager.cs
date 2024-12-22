0d060799e50ae72feacbae74776ddfeb089a5bc5
BioCidee <128076002+BioCidee@users.noreply.github.com>
2024-12-22 10:35:14 +0100

    WIP Build System
    - Add Button for buying Turret

--- Assets/Games/Scripts/Management/Ui_BuildManager.cs
+++ Assets/Games/Scripts/Management/Ui_BuildManager.cs
@@ -6,8 +6,14 @@ public class Ui_BuildManager : MonoBehaviour
 
     [InspectorName("Building")]
     [SerializeField] private GameObject Excavator;
+    [SerializeField] private GameObject Turret;
 
     public void BuildExcavator() {
         mouseLogic.GetBuilding(Excavator);
+    }
+
+    public void BuildTurret()
+    {
+        mouseLogic.GetBuilding(Turret);
     }
 }
