2aa9aa659c14f39872751be2b7718832375a3440
BioCidee <rudy.courtois@hotmail.com>
2025-01-03 15:29:38 +0100

    WIP AI Movement
    - Ai Movement work

--- Assets/Games/Scripts/IA/OVNI/IA_Movement.cs
+++ Assets/Games/Scripts/IA/OVNI/IA_Movement.cs
@@ -3,9 +3,33 @@ using UnityEngine;
 
 public class IA_Movement : MonoBehaviour
 {
+    // System
+    [SerializeField] private IA_DetectionSystem _detectionSystem;
+
     // Targets
     private GameObject target;
     private List<GameObject> listTarget;
 
+    // Movement Parameters
+    private int moveSpeed;
+    private int stopDistance;
+    private float fixedHeight;
 
+    private void Start() {
+        fixedHeight = transform.position.y;
+    }
+
+    private void Update() {
+        listTarget = _detectionSystem.ReturnListThreat();
+    }
+
+    private void GoToTarget() {
+        float distance = Vector3.Distance(transform.position.x, fixedHeight, transform.position.z);
+    }
+
+    private void SetTarget() {
+        if (listTarget.Count != 0) {
+            target = listTarget[0];
+        }
+    }
 }
