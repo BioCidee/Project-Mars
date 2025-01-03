72bb4360d2c36cf1cc11dae759c5f0fc5afeb139
BioCidee <rudy.courtois@hotmail.com>
2025-01-03 11:33:51 +0100

    WIP Detection System OVNI
    - Work on detection system for the OVNI

--- Assets/Games/Scripts/IA/OVNI/DetectionSystem.cs
+++ Assets/Games/Scripts/IA/OVNI/DetectionSystem.cs
@@ -7,16 +7,17 @@ public class DetectionSystem : MonoBehaviour
     private Collider m_Collider;
 
     //Detection
-    private LayerMask buildLayer;
-    private List<GameObject> listObjectsDetected;
-    private List<GameObject> listThreat;
+    [Header("Detection Parameters")]
+    [SerializeField] private LayerMask buildLayer;
+    private List<GameObject> listObjectsDetected = new List<GameObject>();
+    private List<GameObject> listThreat = new List<GameObject>();
 
     private void Start() {
         m_Collider = GetComponent<Collider>();
     }
 
     private void Update() {
-        if (listObjectsDetected != null) {
+        if (listObjectsDetected.Count != 0) {
             SelectionThreat();
         }
     }
@@ -26,12 +27,23 @@ public class DetectionSystem : MonoBehaviour
     }
 
     private void SelectionThreat() {
-        for(int i = listObjectsDetected.Count; i > 0; i--) {
-            if (listObjectsDetected[i].gameObject.layer == buildLayer) {
-                listThreat.Add(listObjectsDetected[i]);
+        for(int i = listObjectsDetected.Count - 1; i >= 0; i--) {
+            if (listObjectsDetected[i].gameObject.layer == 8) {
+                Debug.Log("Start Thread Adding");
+                AddThreat(listObjectsDetected[i]);
             } else {
                 listObjectsDetected.RemoveAt(i);
+                Debug.Log("Detection Removed");
             }
+        }
+    }
+
+    private void AddThreat(GameObject _potentialThreat) {
+        if (listThreat.Contains(_potentialThreat)) {
+            return;
+        } else {
+            listThreat.Add(_potentialThreat);
+            Debug.Log("Add threat");
         }
     }
 }
