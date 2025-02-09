d6041f6963e4548992596df256d09f0f149a2d8d
BioCidee <rudy.courtois@hotmail.com>
2025-02-09 17:21:19 +0100

    WIP Camera Rotation
    - New Key
    - Bug Fixe

--- Assets/Games/Scripts/Maps/Mouse/MouseMouvement.cs
+++ Assets/Games/Scripts/Maps/Mouse/MouseMouvement.cs
@@ -7,7 +7,7 @@ public class MouseMouvement : MonoBehaviour
     [SerializeField] private float cameraMoveSpeed;
     [SerializeField] private float cameraDistance;
 
-    private Vector3 pointZero;
+    [SerializeField] private Vector3 pointZero;
     private Vector3 previousPosition;
 
     private void Start() {
@@ -15,20 +15,20 @@ public class MouseMouvement : MonoBehaviour
     }
 
     private void Update() {
-        SetCameraDistance();
-
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetKeyDown(KeyCode.LeftControl)) {
             previousPosition = mainCamera.ScreenToViewportPoint(Input.mousePosition);
         }
 
-        if (Input.GetMouseButton(0)) {
+        if (Input.GetKey(KeyCode.LeftControl)) {
+            SetCameraDistance();
+
             Vector3 direction = previousPosition - mainCamera.ScreenToViewportPoint(Input.mousePosition);
 
-            mainCamera.transform.position = pointZero;
+            mainCamera.transform.position = new Vector3(pointZero.x, pointZero.y, pointZero.z);
 
             mainCamera.transform.Rotate(new Vector3(cameraMoveSpeed, 0, 0), direction.y * 180);
             mainCamera.transform.Rotate(new Vector3(0,cameraMoveSpeed,0), -direction.x * 180, Space.World);
-            mainCamera.transform.Translate(new Vector3(0,0,-50)); 
+            mainCamera.transform.Translate(new Vector3(0, 0, cameraDistance));
 
             previousPosition = mainCamera.ScreenToViewportPoint(Input.mousePosition);  
         }
@@ -36,8 +36,8 @@ public class MouseMouvement : MonoBehaviour
 
     private void SetCameraDistance() {
         float scrool = Input.GetAxis("Mouse ScrollWheel");
-        cameraDistance += scrool * 5f; // 
-        cameraDistance = Mathf.Clamp(cameraDistance,0, 50); // Limit the camera distance
+        cameraDistance += scrool * 10f; // 
+        cameraDistance = Mathf.Clamp(cameraDistance,-50, 50); // Limit the camera distance
     }
 
 
